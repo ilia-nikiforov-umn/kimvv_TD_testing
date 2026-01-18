d4352510b88f6af842b4b798cef562593d96ff27
ilia Nikiforov <nikif002@umn.edu>
2026-01-18 16:44:24 -0600

    update TD

--- pre_setup.py
+++ pre_setup.py
@@ -25,7 +25,7 @@ OPENKIM_TEST_DRIVERS = {
 
 # List of URLs of development Test Drivers to test
 DEVEL_TEST_DRIVERS = {
-    "FreeEnergies__TD_000000000000_000-000b-ilia0.tar.gz": {
+    "FreeEnergies__TD_000000000000_000-000b-ilia1.tar.gz": {
         "target_size": 100,
         "temperature_K": 300,
     }
